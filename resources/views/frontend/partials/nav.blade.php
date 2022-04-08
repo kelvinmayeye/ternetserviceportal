c529fdb616a78daf55054c3304e6a4b42587956e
kelzbiggie01 <kelzbiggie@gmail.com>
2022-04-08 12:35:28 -0700

    working on logo.png on navbar

--- resources/views/frontend/partials/nav.blade.php
+++ resources/views/frontend/partials/nav.blade.php
@@ -2,7 +2,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav mr-auto">
                 <li class="nav-item hover">
-                    <img src="{{asset('storage/services/logo.png')}}" alt="photo" height="50" width="50" class="mr-2">
+                    <img src="{{asset('img/logo.png')}}" alt="photo" height="50" width="50" class="mr-2">
                 <li class="nav-item hover">
                   <a class="nav-link" href="{{url('/')}}">Home</a>
                 </li>
