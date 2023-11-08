209ae5aa2c25a3e5ba8b6cc0c34cc303d59209ae
kelvinmayeye <kevmayeye97@gmail.com>
2023-11-08 14:38:32 +0300

    feat: adding roles and user roles seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,18 +5,19 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Database\Seeders\DepartmentSeeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
     /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-       $this->call(DepartmentSeeder::class);
-        \App\Models\User::factory(10)->create();
-        \App\Models\Status::factory(1)->create();
-        \App\Models\Service::factory(1)->create();
+    * Seed the application's database.
+    *
+    * @return void
+    */
+
+    public function run() {
+        $this->call( DepartmentSeeder::class );
+        \App\Models\User::factory( 3 )->create();
+        \App\Models\Status::factory( 1 )->create();
+        \App\Models\Service::factory( 1 )->create();
+        $this->call( RoleSeeder::class );
+        $this->call( AdminSeeder::class );
     }
 }
